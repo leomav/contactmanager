458a4a0f871d92e36c960ef699c02d198baaec79
leomav <leo.mav.8@gmail.com>
2020-04-05 14:23:12 +0300

    Updates

---
+++ precache-manifest.b7848095234dfc01de59f9a2f5e514c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c04cbc221d64b7f03cedc0663feba3",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "9f697baad3e116e02920",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1969115b0832b0f1de79",
+    "url": "/contactmanager/static/css/main.a3101490.chunk.css"
+  },
+  {
+    "revision": "9f697baad3e116e02920",
+    "url": "/contactmanager/static/js/2.90fc3eae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.90fc3eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1969115b0832b0f1de79",
+    "url": "/contactmanager/static/js/main.4da4b8cf.chunk.js"
+  },
+  {
+    "revision": "71d3a297f4256315968e",
+    "url": "/contactmanager/static/js/runtime-main.d7d94ad5.js"
+  }
+]);
